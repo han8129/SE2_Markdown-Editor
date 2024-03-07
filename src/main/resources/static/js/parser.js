dfa2385bb8182227a62ff57161da4907de113be8
han8129 <110092300+han8129@users.noreply.github.com>
2024-03-07 21:48:28 +0700

    Markdown parser demo

--- src/main/resources/static/js/parser.js
+++ src/main/resources/static/js/parser.js
@@ -1,10 +1,42 @@
+ function headingParse(str) {
+    if (false == /^#{1,6}\s+\w+/.test(str))
+    {
+        return str;
+    }
+
+    let level = 0;
+
+   for (const idx in str)
+   {
+        if (str[idx] == '#')
+        {
+            level += 1
+        }
+   }
+
+   const firstCharIdx = str.search(/[^# ]/)
+   console.log(firstCharIdx);
+   const tag = `h${level}`;
+   const bareText = str.slice(firstCharIdx);
+   return `<${tag}>${bareText}</${tag}>`
+ }
+
 /**
  * Convert a string containing Markdown syntax into corresponding HTML representation.
  * @param {string} str - A string with Markdown syntax .
  * @return {string} A valid html string.
  */
  function parse(str) {
-  return "";
+   let htmlStr = "";
+   const splitedStr =  str.split('\n');
+
+   if (splitedStr.length == 1) return headingParse(str);
+   for (const line in splitedStr)
+   {
+    htmlStr += headingParse(line);
+   }
+
+  return htmlStr;
 }
 
 module.exports = parse;
