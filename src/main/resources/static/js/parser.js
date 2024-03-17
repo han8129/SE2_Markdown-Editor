6e4c7c4a356baf13f5ca21076abe1fa5d983f87c
phuongN4m <trankhangbla@gmail.com>
2024-03-17 17:30:45 +0700

    update parser.js

--- src/main/resources/static/js/parser.js
+++ src/main/resources/static/js/parser.js
@@ -3,9 +3,8 @@
  * @param {string} str - A string with Markdown syntax .
  * @return {string} A valid html string.
  */
- function parse(str) {
-  return "";
-}
-
-module.exports = parse;
-
+function parse(str) {
+    return "";
+  }
+  
+  module.exports = parse;
